955f56f01de7c7137effc6ae354a334f7205a50d
noa-ye <noayesha@post.bgu.ac.il>
2023-05-12 20:15:39 +0300

    SimpleCompressorOutputStream & SimpleDecompressorInputStream

--- src/IO/SimpleCompressorOutputStream.java
+++ src/IO/SimpleCompressorOutputStream.java
@@ -1,4 +1,71 @@
 package IO;
 
-public class SimpleCompressorOutputStream {
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class SimpleCompressorOutputStream extends OutputStream {
+    private OutputStream out; // the underlying output stream
+    private int currentByte; // the current byte being processed
+    private int count; // the number of occurrences of the current byte
+
+
+
+    /**
+     * @param out
+     * The SimpleCompressorOutputStream class extends the OutputStream class.
+     * It is used to compress a maze into a byte array.
+     * The compression is done by counting the number of occurrences of each byte value in a row.
+     * The compressed maze is written to the underlying output stream.
+     */
+
+    public SimpleCompressorOutputStream(OutputStream out) {
+        this.out = out; // the underlying output stream
+        this.currentByte = 0; // the current byte being processed
+        this.count = 0; // the number of occurrences of the current byte
+    }
+
+    /**
+     *
+     * The write method is overridden from the OutputStream class. It writes a single byte to the output stream.
+     * If the byte b is different from the current byte, it means a new byte is encountered.
+     * In that case, it writes the count of the previous byte occurrences to the output stream, sets the current byte to b, and resets the count to 1.
+     * If the byte b is the same as the current byte, it increments the count. If the count reaches 255 (the maximum value for a byte),
+     * it writes the count to the output stream and resets it to 0.
+     */
+    public void write(int b) throws IOException {
+        try {
+            if (b != currentByte) { // if the byte value is different from the current byte
+                out.write((byte) count); // write the number of occurrences of the current byte
+                currentByte = b; // update the current byte
+                count = 1; // reset the number of occurrences of the current byte
+            } else {
+                count++; // increment the number of occurrences of the current byte
+                if (count == 255) { // if the number of occurrences of the current byte is 255
+                    out.write((byte) count); // write the number of occurrences of the current byte
+                    count = 0; // reset the number of occurrences of the current byte
+                }
+            }
+        } catch (IOException e) {}
+    }
+
+    public void write(byte [] b) { // write the compressed maze to the underlying output stream
+        try {
+            for (int i = 0; i < 10; i++) { // write the first 10 bytes of the maze
+                out.write(b[i]); // write the byte
+            }
+            for(int i = 10; i < b.length; i++){ // write the rest of the maze
+                write(b[i]); // write the byte
+            }
+            if(count !=0){ // if the number of occurrences of the current byte is not 0
+                out.write(count); // write the number of occurrences of the current byte
+            }
+        } catch (IOException e) {}
+    }
 }
+
+/**
+ * By processing one byte at a time, the method (write(int b)) can keep track of the current byte being processed and count the number of occurrences of that byte in a row.
+ * This allows for efficient compression by storing the count of consecutive occurrences instead of repeating the same byte multiple times.
+ * The write(byte[] b) method is responsible for writing the compressed maze to the output stream. It iterates over the byte array representing the maze and calls the
+ * write(int b) method for each byte. This ensures that the compression logic is applied to each byte in the maze individually, resulting in an optimized compressed representation.
+ */
--- src/IO/SimpleDecompressorInputStream.java
+++ src/IO/SimpleDecompressorInputStream.java
@@ -1,4 +1,59 @@
 package IO;
 
-public class SimpleDecompressorInputStream {
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class SimpleDecompressorInputStream extends InputStream {
+    private InputStream in; // the underlying input stream
+    int currerntBit; // the current bit
+    int count; // the current count
+
+    public SimpleDecompressorInputStream(InputStream in) {
+        this.in = in; // the underlying input stream
+        currerntBit = 0;
+        count = 0;
+    }
+
+    /**
+     * The read() method is overridden from the InputStream class.
+     * Inside the method, it tries to read a single byte from the underlying input stream in using the read() method.
+     * If an IOException occurs during the read operation, it catches the exception and returns 0.
+     * Otherwise, it returns the byte value read from the input stream.
+     */
+    @Override
+    public int read() throws IOException {
+        try {
+            return in.read();
+        } catch (IOException e) {}
+        return 0;
+    }
+
+    /**
+     The read() method reads bytes from the input stream and decompresses them into the provided byte array b.
+     It starts by copying the first 10 bytes as-is. Then, it reads pairs of bytes representing the count and value, replicating the value byte count
+     times in the array b. Finally, it alternates the bit value for each pair of bytes to achieve the decompression. The function returns the number of bytes read
+     and stored in the array b
+     */
+    @Override
+    public int read(byte[] b) throws IOException { // read the compressed maze from the underlying input stream
+        for( int i = 0; i < 10; i++) { // read the first 10 bytes of the maze
+            b[i] = (byte)read();
+        }
+        int i = 10; // the index of the array b
+        while((count = read()) != -1){ // read the count of the current byte
+            while(count > 0){ // while the count is positive
+                b[i] = (byte)currerntBit; // write the current bit to the array b
+                count--; // decrement the count
+                i++;
+            }
+            if(currerntBit == 0){ // if the current bit is 0
+                currerntBit= 1; // set the current bit to 1.
+            }
+            else{ // if the current bit is 1
+                currerntBit= 0; // set the current bit to 0
+            }
+        }
+        return i;
+    }
 }

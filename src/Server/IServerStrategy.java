3be6fb38e8e77a06ef604a68f32e0c014a333c8a
lioraft <lioraftabi@gmail.com>
2023-05-19 00:24:13 +0300

    server class and server strategy interface as taught in practice

--- src/Server/IServerStrategy.java
+++ src/Server/IServerStrategy.java
@@ -1,4 +1,14 @@
 package Server;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+
+
+/**
+ * The IServerStrategy interface is responsible for the server's strategy.
+ * It receives an input stream and an output stream.
+ * The strategy will be applied on the input stream and the output stream.
+ */
 public interface IServerStrategy {
+    void applyStrategy(InputStream inFromClient, OutputStream outToClient);
 }
--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,4 +1,60 @@
 package Server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+
+/**
+ * The Server class is responsible for the server's logic.
+ * It receives a port, a listening interval and a strategy.
+ * The server will listen to the port and accept clients.
+ * For each client, the server will apply the strategy.
+ * The server will stop when the stop() method is called.
+ */
+
 public class Server {
+    private int port; // port number
+    private int listeningIntervalMS; // listening interval
+    private IServerStrategy strategy; // strategy to be applied with each client
+    private boolean stop; // boolean to stop the server
+
+    // constructor of server
+    public Server(int port, int listeningIntervalMS, IServerStrategy strategy) {
+        this.port = port;
+        this.listeningIntervalMS = listeningIntervalMS;
+        this.strategy = strategy;
+    }
+
+    // start the server
+    public void start(){
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            serverSocket.setSoTimeout(listeningIntervalMS);
+            System.out.println("Starting server at port = " + port);
+
+            while (!stop) {
+                try {
+                    Socket clientSocket = serverSocket.accept();
+                    System.out.println("Client accepted: " + clientSocket.toString());
+
+                    try {
+                        strategy.applyStrategy(clientSocket.getInputStream(), clientSocket.getOutputStream());
+                        clientSocket.close();
+                    } catch (IOException e){
+                        e.printStackTrace();
+                    }
+                } catch (SocketTimeoutException e){
+                    System.out.println("Socket timeout");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // stop the server
+    public void stop(){
+        stop = true;
+    }
 }

ff87b9149cd0cfcafa123cfcc1f8986af8cbe364
lioraft <lioraftabi@gmail.com>
2023-05-19 13:40:09 +0300

    modified server to accept multiple clients using threadpool

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -24,31 +24,43 @@ public class Server {
 
     // constructor of server
     public Server(int port, int listeningIntervalMS, IServerStrategy strategy) {
-        this.port = port;
-        this.listeningIntervalMS = listeningIntervalMS;
-        this.strategy = strategy;
-        this.executor = Executors.newFixedThreadPool(Configurations.getInstance().getThreadPoolSize());
+        this.port = port; // initialize the port
+        this.listeningIntervalMS = listeningIntervalMS; // initialize the listening interval
+        this.strategy = strategy; // initialize the strategy
+        this.executor = Executors.newFixedThreadPool(Configurations.getInstance().getThreadPoolSize()); // initialize the thread pool
     }
 
     // start the server
     public void start(){
         try {
+            // initialize server socket based on the port, and set time out based on the listening interval
             ServerSocket serverSocket = new ServerSocket(port);
             serverSocket.setSoTimeout(listeningIntervalMS);
+            // let user know the server is starting
             System.out.println("Starting server at port = " + port);
 
+            // while the server is not stopped, accept clients and apply the strategy
             while (!stop) {
-                try {
+                try { // try to accept a client
                     Socket clientSocket = serverSocket.accept();
                     System.out.println("Client accepted: " + clientSocket.toString());
 
                     try {
-                        strategy.applyStrategy(clientSocket.getInputStream(), clientSocket.getOutputStream());
-                        clientSocket.close();
+                        // apply the strategy with the client, using the thread pool, and close the client socket
+                        Runnable runnable = () -> {
+                            try {
+                                strategy.applyStrategy(clientSocket.getInputStream(), clientSocket.getOutputStream());
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                        };
+                        executor.execute(runnable); // execute the strategy
+                        clientSocket.close(); // close socket when finish
+                        executor.shutdown(); // shutdown the thread pool when finish
                     } catch (IOException e){
                         e.printStackTrace();
                     }
-                } catch (SocketTimeoutException e){
+                } catch (SocketTimeoutException e){ // if the server times out, let user know
                     System.out.println("Socket timeout");
                 }
             }

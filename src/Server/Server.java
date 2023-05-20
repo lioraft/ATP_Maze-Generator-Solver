0840e2ed1a7499abba4130497e8f8f4abc9ddca0
lioraft <lioraftabi@gmail.com>
2023-05-20 23:15:16 +0300

    now everything works while being multithreaded

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,83 +1,83 @@
 package Server;
 
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-
-
-/**
- * The Server class is responsible for the server's logic.
- * It receives a port, a listening interval and a strategy.
- * The server will listen to the port and accept clients.
- * For each client, the server will apply the strategy.
- * The server will stop when the stop() method is called.
- */
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class Server {
-    private int port; // port number
-    private int listeningIntervalMS; // listening interval
-    private IServerStrategy strategy; // strategy to be applied with each client
-    private volatile boolean stop;
-    private final Logger LOG = LogManager.getLogger(); //Log4j2
+    private int port; // port of the server
+    private int listeningIntervalMS; // listening interval of the server
+    private IServerStrategy strategy; // strategy of the server
+    private volatile boolean stop; // boolean variable to stop the server
+    private final Logger LOG = LogManager.getLogger(); // Log4j2 - for logging
+    private ThreadPoolExecutor threadPool; // Thread pool
 
-    // thread pool. each time a server is being initialized, it will use a thread pool
-    private ThreadPoolExecutor threadPool;
 
-    // constructor of server
+    // constructor - initialize the server
     public Server(int port, int listeningIntervalMS, IServerStrategy strategy) {
-        this.port = port; // initialize the port
-        this.listeningIntervalMS = listeningIntervalMS; // initialize the listening interval
-        this.strategy = strategy; // initialize the strategy
-        threadPool = (ThreadPoolExecutor) Executors.newCachedThreadPool(); // initialize the thread pool
+        this.port = port;
+        this.listeningIntervalMS = listeningIntervalMS;
+        this.strategy = strategy;
+        this.stop = false;
+        threadPool = (ThreadPoolExecutor) Executors.newFixedThreadPool(Configurations.getInstance().getThreadPoolSize()); // initialize the thread pool
     }
 
     // start the server
-    public void start(){
-        try {
-            stop = false; // initialize stop to false
-            ServerSocket serverSocket = new ServerSocket(port);
-            serverSocket.setSoTimeout(listeningIntervalMS);
-            LOG.info("Starting server at port = " + port);
-            while (!stop) {
-                // accept new clients
-                Socket clientSocket = serverSocket.accept();
-                LOG.info("Client accepted: " + clientSocket.toString());
-                // create new client thread
-                Thread clientThread = new Thread(() -> {
-                    handleClient(clientSocket);
-                });
-                threadPool.execute(clientThread); // execute the client handler in the thread pool
-            }
-        } catch (Exception e) {
-            LOG.error("Failed to execute connection");
-        }
-        finally {
-            // check if there are active threads in the thread pool
-                if (threadPool.getActiveCount() == 0) // if there are no active threads in the thread pool, shut it down
-                    threadPool.shutdownNow();
-            }
-        }
+    public void start() {
+        // try to make new connection, as runnable so it won't block the main thread
+        Runnable runnable = () -> {
+            try {
+                // create new server socket
+                ServerSocket serverSocket = new ServerSocket(port);
+                serverSocket.setSoTimeout(listeningIntervalMS);
+                LOG.info("Starting server at port = " + port);
 
+                // while the server is not stopped
+                while (!stop) {
+                    try {
+                        // accept new client
+                        Socket clientSocket = serverSocket.accept();
+                        LOG.info("Client accepted: " + clientSocket.toString());
+
+                        // The thread pool will handle the new Client in a new thread
+                        threadPool.execute(() -> {
+                            handleClient(clientSocket);
+                        });
+
+                    } catch (SocketTimeoutException e) { // if the server timed out
+                        LOG.debug("Socket timeout");
+                    }
+                }
+            } catch (IOException e) { // if there was an error with the server socket
+                LOG.error("IOException", e);
+            }
+        };
+        new Thread(runnable).start(); // Start the server thread
+    }
+
+    // this function is for handling the client in a separate thread. the start function will call this function in a new thread
     private void handleClient(Socket clientSocket) {
         try {
+            // apply the strategy on the client
             strategy.applyStrategy(clientSocket.getInputStream(), clientSocket.getOutputStream());
             LOG.info("Done handling client: " + clientSocket.toString());
+            // close the client socket
             clientSocket.close();
         } catch (IOException e){
             LOG.error("IOException", e);
         }
     }
 
-
-    // stop the server
+    // this function is for stopping the server
     public void stop(){
         LOG.info("Stopping server...");
-        stop = true; // stop the server
-        threadPool.shutdown(); // shut down the thread pool
+        stop = true; // change the indicator to true
+        threadPool.shutdown(); // shut down pool
     }
 }

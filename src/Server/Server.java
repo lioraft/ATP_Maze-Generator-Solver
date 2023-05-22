6d4fe5f6f3691aed360ab75db58733be2cdd8605
lioraft <lioraftabi@gmail.com>
2023-05-22 18:46:49 +0300

    deleted log and corrected prints to screen

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,8 +1,5 @@
 package Server;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -15,7 +12,6 @@ public class Server {
     private int listeningIntervalMS; // listening interval of the server
     private IServerStrategy strategy; // strategy of the server
     private volatile boolean stop; // boolean variable to stop the server
-    private final Logger LOG = LogManager.getLogger(); // Log4j2 - for logging
     private ThreadPoolExecutor threadPool; // Thread pool
 
 
@@ -33,29 +29,27 @@ public class Server {
         // try to make new connection, as runnable so it won't block the main thread
         Runnable runnable = () -> {
             try {
-                // create new server socket
+                // create new server socket and connect to it
                 ServerSocket serverSocket = new ServerSocket(port);
                 serverSocket.setSoTimeout(listeningIntervalMS);
-                LOG.info("Starting server at port = " + port);
 
                 // while the server is not stopped
                 while (!stop) {
                     try {
                         // accept new client
                         Socket clientSocket = serverSocket.accept();
-                        LOG.info("Client accepted: " + clientSocket.toString());
 
                         // The thread pool will handle the new Client in a new thread
                         threadPool.execute(() -> {
                             handleClient(clientSocket);
                         });
 
-                    } catch (SocketTimeoutException e) { // if the server timed out
-                        LOG.debug("Socket timeout");
+                    } catch (SocketTimeoutException e) { // if the server timed out, print error message
+                        System.out.println("Socket timeout");
                     }
                 }
-            } catch (IOException e) { // if there was an error with the server socket
-                LOG.error("IOException", e);
+            } catch (IOException e) { // if there was an error with the server socket, print error message
+                System.out.println(e.getMessage());
             }
         };
         new Thread(runnable).start(); // Start the server thread
@@ -66,17 +60,15 @@ public class Server {
         try {
             // apply the strategy on the client
             strategy.applyStrategy(clientSocket.getInputStream(), clientSocket.getOutputStream());
-            LOG.info("Done handling client: " + clientSocket.toString());
             // close the client socket
             clientSocket.close();
         } catch (IOException e){
-            LOG.error("IOException", e);
+            System.out.println(e.getMessage());
         }
     }
 
     // this function is for stopping the server
     public void stop(){
-        LOG.info("Stopping server...");
         stop = true; // change the indicator to true
         threadPool.shutdown(); // shut down pool
     }
